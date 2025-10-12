5785742ebc2bd25766a418a519d6dab966e7256f
nikolay-mansas <dokykin0nikolay@gmail.com>
2025-10-12 13:20:21 +0500

    Add redirect when press buttons

--- src/lib/components/AuthNotice.svelte
+++ src/lib/components/AuthNotice.svelte
@@ -5,7 +5,7 @@
 {#if visible}
 	<div class="bg-[#1a1124] border border-fuchsia-700 text-fuchsia-200 px-4 py-2 text-sm flex items-center justify-between">
 		<span>
-			Чтобы сохранять историю и прогресс просмотра — <b class="text-fuchsia-400">войдите в аккаунт</b>.
+			Чтобы сохранять историю и прогресс просмотра — <a class="text-fuchsia-400 font-bold" href="/login">войдите в аккаунт</a>.
 		</span>
 		<button
 			class="ml-4 text-fuchsia-400 hover:text-fuchsia-500 transition"
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
 	import '../app.css';
+	import { goto } from '$app/navigation'
 	import favicon from '$lib/assets/favicon.svg';
 	import AuthNotice from '$lib/components/AuthNotice.svelte';
 
 	let { children } = $props();
+
+	function OpenLoginPage() {
+		goto('login');
+	}
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel='icon' href={favicon} />
 </svelte:head>
 
 <AuthNotice />
 
-<main class="min-h-screen bg-[#0a0a12] text-white font-sans">
-	<header class="flex justify-between items-center px-4 py-4 border-b border-white/10">
-		<h1 class="text-xl sm:text-2xl font-bold text-white tracking-wide">
-			<span class="text-purple-400">β</span> AnimeViewer
+<main class='min-h-screen bg-[#0a0a12] text-white font-sans'>
+	<header class='flex justify-between items-center px-4 py-4 border-b border-white/10'>
+		<h1 class='text-xl sm:text-2xl font-bold text-white tracking-wide'>
+			<span class='text-purple-400'>β</span> AnimeViewer
 		</h1>
-		<button class="btn-custom">
+
+		<button class='btn-custom' onclick={OpenLoginPage}>
 			Вход
 		</button>
 	</header>
