b9ac5af65696a4c0e3189247f346d86084c55880
nikolay-mansas <dokykin0nikolay@gmail.com>
2025-12-21 16:35:30 +0500

    Update email in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
 <footer class="mt-12 px-4 pb-6 text-center text-xs text-gray-500">
 	<hr class="my-4 border-gray-700" />
-	<p>Contact: report@null.com | Kira is Best!</p>
+	<p>Contact: report@animeviewer.ru | Kira is Best!</p>
 </footer>
